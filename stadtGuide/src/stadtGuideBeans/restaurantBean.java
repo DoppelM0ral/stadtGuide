64f3efb5f939f3e3582be25dd3ae7050175cde4a
Carolina Jochim <Carolina Jochim@LAPTOP-FIOORBQL.fritz.box>
2023-09-15 17:13:21 +0200

    beans upload

--- stadtGuide/src/stadtGuideBeans/restaurantBean.java
+++ stadtGuide/src/stadtGuideBeans/restaurantBean.java
@@ -1,6 +1,5 @@
 package stadtGuideBeans;
 
-import java.io.Console;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,7 +9,110 @@ import jdbc.NoConnectionException;
 import jdbc.PostgreSQLAccess;
 
 public class restaurantBean {
+	String newRestName;
+	String newRestCity;
+	String newRestPlz;
+	String newRestFood;
+	String nourish;
+	String restPreis;
+	
+	Connection dbConn;
 
-	String test;
+	public restaurantBean() throws NoConnectionException {
+		this.dbConn = new PostgreSQLAccess().getConnection();
+		this.initialize();
+	}
+	
+	public void initialize() {
+		this.newRestName = "";
+		this.newRestCity = "";
+		this.newRestPlz = "";
+		this.newRestFood = "";
+		this.nourish = "";
+		this.restPreis = "";
+	}
+	
+	public boolean restaurantCheck() throws SQLException{
+		//true - Stadt existiert - liegt in der Datenbank vor
+		//false - Stadt existiert - liegt nicht in der Datenbank vor
+		String sql = "select name from addons where plz = ?";
+		System.out.println(sql);
+		PreparedStatement prep = this.dbConn.prepareStatement(sql);
+		prep.setString(1, this.newRestPlz);
+		ResultSet dbRes = prep.executeQuery();
+		boolean check = dbRes.next();
+		return check;
+	}
+	
+	public void restaurantAnlegen() throws SQLException {
+		String sql = "insert into addons (name, stadt, plz, art, kategorie) values (?,?,?,?,?)";
+		System.out.println(sql);
+		PreparedStatement prep = this.dbConn.prepareStatement(sql);
+		prep.setString(1, this.newRestName);
+		prep.setString(2, this.newRestCity);
+		prep.setString(3, this.newRestPlz);
+		prep.setString(4, this.newRestFood);
+		prep.setString(5, "Restaurant");
+		prep.setString(6, this.restPreis);
+		prep.executeUpdate();
+		System.out.println();
+	}
+	
+	public Connection getDbConn() {
+		return dbConn;
+	}
+
+	public void setDbConn(Connection dbConn) {
+		this.dbConn = dbConn;
+	}
+
+	public String getNewRestName() {
+		return newRestName;
+	}
+
+	public void setNewRestName(String newRestName) {
+		this.newRestName = newRestName;
+	}
+
+	public String getNewRestCity() {
+		return newRestCity;
+	}
+
+	public void setNewRestCity(String newRestCity) {
+		this.newRestCity = newRestCity;
+	}
+
+	public String getNewRestPlz() {
+		return newRestPlz;
+	}
+
+	public void setNewRestPlz(String newRestPlz) {
+		this.newRestPlz = newRestPlz;
+	}
+
+	public String getNewRestFood() {
+		return newRestFood;
+	}
+
+	public void setNewRestFood(String newRestFood) {
+		this.newRestFood = newRestFood;
+	}
+
+	public String getNourish() {
+		return nourish;
+	}
+
+	public void setNourish(String nourish) {
+		this.nourish = nourish;
+	}
+
+	public String getRestPreis() {
+		return restPreis;
+	}
+
+	public void setRestPreis(String restPreis) {
+		this.restPreis = restPreis;
+	}
+	
 	
 }
--- stadtGuide/src/stadtGuideBeans/unterkunftBean.java
+++ stadtGuide/src/stadtGuideBeans/unterkunftBean.java
@@ -1,6 +1,5 @@
 package stadtGuideBeans;
 
-import java.io.Console;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,8 +9,99 @@ import jdbc.NoConnectionException;
 import jdbc.PostgreSQLAccess;
 
 public class unterkunftBean {
+	String newStayName;
+	String newStayCity;
+	String newStayPlz;
+	String newStay;
+	String stayPreis;
 	
-	String test;
+	Connection dbConn;
 
+	public unterkunftBean() throws NoConnectionException {
+		this.dbConn = new PostgreSQLAccess().getConnection();
+		this.initialize();
+	}
+	
+	public void initialize() {
+		this.newStayName = "";
+		this.newStayCity = "";
+		this.newStayPlz = "";
+		this.newStay = "";
+		this.stayPreis = "";
+	}
+	
+	public boolean unterkunftCheck() throws SQLException{
+		//true - Stadt existiert - liegt in der Datenbank vor
+		//false - Stadt existiert - liegt nicht in der Datenbank vor
+		String sql = "select name from addons where plz = ?";
+		System.out.println(sql);
+		PreparedStatement prep = this.dbConn.prepareStatement(sql);
+		prep.setString(1, this.newStayPlz);
+		ResultSet dbRes = prep.executeQuery();
+		boolean check = dbRes.next();
+		return check;
+	}
+	
+	public void unterkunftAnlegen() throws SQLException {
+		String sql = "insert into addons (name, stadt, plz, art, kategorie, preisklasse) values (?,?,?,?,?,?)";
+		System.out.println(sql);
+		PreparedStatement prep = this.dbConn.prepareStatement(sql);
+		prep.setString(1, this.newStayName);
+		prep.setString(2, this.newStayCity);
+		prep.setString(3, this.newStayPlz);
+		prep.setString(4, this.newStay);
+		prep.setString(5, "Unterkunft");
+		prep.setString(6, this.stayPreis);
+		prep.executeUpdate();
+		System.out.println();
+	}
+
+	public String getNewStayName() {
+		return newStayName;
+	}
+
+	public void setNewStayName(String newStayName) {
+		this.newStayName = newStayName;
+	}
+
+	public String getNewStayCity() {
+		return newStayCity;
+	}
+
+	public void setNewStayCity(String newStayCity) {
+		this.newStayCity = newStayCity;
+	}
+
+	public String getNewStayPlz() {
+		return newStayPlz;
+	}
+
+	public void setNewStayPlz(String newStayPlz) {
+		this.newStayPlz = newStayPlz;
+	}
+
+	public String getNewStay() {
+		return newStay;
+	}
+
+	public void setNewStay(String newStay) {
+		this.newStay = newStay;
+	}
+
+	public String getStayPreis() {
+		return stayPreis;
+	}
+
+	public void setStayPreis(String stayPreis) {
+		this.stayPreis = stayPreis;
+	}
+
+	public Connection getDbConn() {
+		return dbConn;
+	}
+
+	public void setDbConn(Connection dbConn) {
+		this.dbConn = dbConn;
+	}
 	
 }
